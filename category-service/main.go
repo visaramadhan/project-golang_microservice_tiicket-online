16c44643916da6aab36b04fd53c6bbc23c04b425
visa28 <visaramadhan6@gmail.com>
2024-12-24 17:03:45 +0700

    test

--- category-service/main.go
+++ category-service/main.go
@@ -1,12 +1,1 @@
 package main
-
-import ()
-
-func main() {
-	lis, err := net.Listen("top", ":8080")
-	if err != nil {
-		panic(err)
-	}
-	grpcServer := grpc.NewServer()
-	pb.CategoryServer(grpcServer, &service.CategoryService)
-}
